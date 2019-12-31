0544fc32f9c6d86edd04451193b209e63bba176f
Justin Baum <justin.baum@ninefx.com>
2019-12-31 16:19:35 -0500

    Finish Dijkstras

--- pineapple-service/solutions/solution.py
+++ pineapple-service/solutions/solution.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python3
 
+INF = float('inf')
 def getLine(func):
     return list(map(func, input().split()))
 
@@ -20,9 +21,30 @@ def makeGraph(nodes, edges, graph):
         graph[na][nb] = edge[2]
     return
 
-def dijkstras(visited, distance, start, current, goal):
-    pass
+def dijkstras(graph, source):
+    n = len(graph)
+    q = [i for i in range(n)]
+    dist = [INF for _ in range(n)]
+    dist[source] = 0
+    while False in q:
+        # Get minimum dist source -> u
+        min_ = INF
+        u = -1
+        for node in q:
+            if dist[node] < min_:
+                min_ = dist[node]
+                u = node
+        if u == -1: break # Dead path
+        q.remove(u)
+        for node in q:
+            if graph[u][node] != None:
+                alt = dist[u] + graph[u][node]
+                if alt < dist[node]:
+                    dist[node] = alt
+    return dist
 
+
+# Input problem
 line = getLine(int)
 n = line[0]
 m = line[1]
@@ -32,9 +54,21 @@ edges = [getEdge() for _ in range(m)]
 path = getLine(str)
 
 
+# Make Adjacency Matrix
 graph = [[None for _ in range(n)] for _ in range(n)]
 makeGraph(nodes, edges, graph)
-print(nodes)
-print(edges)
-print(path)
-print(graph)
+
+# Make precomputed Dijkstras Matrix
+efficientGraph = [dijkstras(graph, i) for i in range(n)]
+
+# Calculate Costs
+cost = 0
+
+prev = path[0]
+cost = 0
+for next_ in path:
+    i = nodes.index(prev)
+    j = nodes.index(next_)
+    cost += efficientGraph[i][j]
+    prev = next_
+print(cost)
