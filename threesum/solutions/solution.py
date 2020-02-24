5fe3b9f2ccb6c7a9f0daad0db59fffe615c088ea
Justin <justin.baum@ninefx.com>
2020-02-24 01:10:30 -0500

    Working on it, O(n^2)

---
+++ threesum/solutions/solution.py
@@ -0,0 +1,22 @@
+def threesum(listy):
+    listy.sort()
+    l = len(listy)
+    # O(n^2)
+    setty = set()
+    for i in range(l-2):
+        j = i+1
+        k = l-1
+        while k > j:
+            target = -listy[i]
+            if(listy[j] + listy[k] == target):
+                setty.add((listy[i], listy[j], listy[k]))
+                j += 1 
+            elif(listy[j] + listy[k] > target):
+                k -= 1
+            else:
+                j += 1
+    return setty
+
+n = int(input())
+nums = list(map(int, input().split(" ")))
+print(len(threesum(nums)))
