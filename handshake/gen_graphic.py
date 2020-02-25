40133bbcd59a5ef395d5815b3152dfd00570d637
Hunter Damron <hdamron1594@yahoo.com>
2020-02-24 23:32:05 -0500

    Switched kalingrad to provide edges as raw inputs

--- handshake/gen_graphic.py
+++ handshake/gen_graphic.py
@@ -2,38 +2,28 @@ from graphviz import Graph
 from sys import argv
 import os
 
-# g = Graph()
-# g.attr('node', shape='circle', fixedsize='true', width='0.5')
-# g.node('1')
-# g.node('2')
-# g.edge('1','2')
-# g.view()
-
-p = 2**31-1
-
 def graphic(i):
     os.system('mkdir -p fig')
 
     with open("testcases/input/input%02d.txt" % i) as f:
         n, m = (int(x) for x in next(f).split())
+        edges = [next(f).strip().split(" and ") for _ in range(m)]
 
     g = Graph()
-    g.attr('node', shape='circle', fixedsize='true', width='0.4')
 
-    nodes = {x*x % p for x in range(1,m+1)}
-    nodes.update({2*x % p for x in nodes})
+    nodes = {e[0] for e in edges}.union({e[1] for e in edges})
     for node in sorted(nodes):
-        g.node('%d' % node)
+        g.node(node)
 
-    for x in range(1,m+1):
-        g.edge('%d' % (x*x % p), '%d' % (2*x*x % p))
+    for p1, p2 in edges:
+        g.edge(p1, p2)
 
-    g.attr('node', shape='circle', fixedsize='true', width='0.1')
-    print(n)
+    g.attr('node', shape='circle', fixedsize='true', width='0.3')
     for x in range(n - len(nodes)):
-        g.node('u%d' % x, '')
+        g.node('_%d' % x, '')
 
-    g.render("fig/input%02d" % i, format="pdf", cleanup=True)
+    g.render('build/input%02d' % i, format='pdf', cleanup=True)
+    os.system('convert -density 600 build/input%02d.pdf fig/input%02d.png' % (i, i))
 
 if __name__ == "__main__":
     i = int(argv[1]) if len(argv) > 1 else 0  # Default to 0
--- handshake/gen_samples.py
+++ handshake/gen_samples.py
@@ -2,32 +2,46 @@
 
 import random
 import os
+from math import floor, ceil, sqrt
 from solution import *
 
-samples = ["4 2",
-           "9 4"]
+samples = [(4, ["J. Cooper and E. Czabarka", "L. Szekely and P. Erdos"]),
+           (6, ["L. Szekely and E. Czabarka", "L. Szekely and P. Erdos", "J. Cooper and P. Erdos", "E. Czabarka and P. Erdos", "J. Cooper and I. Rekleitis"])]
+
+def triangular_index(n, k):
+    # https://stackoverflow.com/a/27088560
+    i = n - 2 - floor(sqrt(-8*k + 4*n*(n-1)-7)/2.0 - 0.5)
+    j = k + i + 1 - n*(n-1)//2 + (n-i)*((n-i)-1)//2
+    return (i,j)
 
 if __name__ == "__main__":
     random.seed(42)
-    for i in range(20):
-        m = random.choice(range(1,1<<(i+10)))
-        # TODO if we can lower uppwer bound, then avg should be able to go above 1
-        n = random.choices([m*2, random.randint(m*2, min(m*10, 10**12-1))], [1,20])[0]
-        samples.append("%d %d" % (n, m))
+    for i in range(5):
+        if i == 0:
+            m = random.randint(1,50)
+        else:
+            m = random.choice(range(1,1<<(i+20)))
+        min_n = 1 + ceil(sqrt(2*m))
+        n = random.randint(min_n, 10*m)
+        max_i = n * (n-1) // 2
+        edges_i = random.sample(range(max_i), m)
+        ijs = [triangular_index(n, k) for k in edges_i]
+        edges = ["%d Smith and %d Johnson" % ij for ij in ijs]
+        samples.append((n, edges))
 
     assert len(samples) < 100, "Too many samples"
     for i, sample in enumerate(samples):
         ifname = "testcases/input/input%02d.txt" % i
         ofname = "testcases/output/output%02d.txt" % i
 
-        n, m = (int(x) for x in sample.split())
-        r = solve(n, m)
-        print("%d %d -> %s" % (n, m, r))
+        n, edges = sample
+        r = solve(n, len(edges))
+        print("%d %d -> %s" % (n, len(edges), r))
         # TODO add brute force for small inputs
 
         os.system("mkdir -p testcases/input testcases/output")
         with open(ifname, 'w+') as ifile, open(ofname, 'w+') as ofile:
-            ifile.write("%s\n" % sample)
+            ifile.write("%d %d\n%s\n" % (n, len(edges), "\n".join(edges)))
             ofile.write(str(r) + '\n')
 
     os.system("zip -r handshake-testcases.zip testcases")
